e06aac5c322254816299c14f9176646512b1113b
Alexander Turenko <totktonada.ru@gmail.com>
2015-04-07 00:03:14 +0300

    Add extra blank lines between top level func (PEP8)

--- parse.py
+++ parse.py
@@ -4,6 +4,7 @@ import requests
 from requests.exceptions import RequestException, BaseHTTPError
 import lxml.html
 
+
 def get_page(url, utf8 = False):
     try:
         r = requests.get(url)
@@ -17,11 +18,13 @@ def get_page(url, utf8 = False):
         r.encoding = 'utf-8'
     return r.text
 
+
 def get_title(url):
     html = get_page(url)
     doc = lxml.html.document_fromstring(html)
     ts = doc.xpath('//title')
     return ts[0].text.strip() if len(ts) >= 1 else 'TODO'
+
 
 # assume 'context_url' are full url
 def full_url(url, context_url):
@@ -42,6 +45,7 @@ def full_url(url, context_url):
         # 'smth.html', './smth.html', or '../smth.html'?
         raise NameError('bad url in \'full_url\':\nurl: ' + url + '\n')
 
+
 # http://stackoverflow.com/a/24151860/1598057
 def innerHTML(node, strip = True):
     res = node.text or ''
@@ -51,15 +55,18 @@ def innerHTML(node, strip = True):
         res = res.strip()
     return res
 
+
 def process_footnotes(s):
     for fn in s['footnotes']:
         s['res'] += '[^%s]: %s' % (fn['num'], fn['body']) + s['par_sep']
     s['footnotes'] = []
 
+
 def process_toplevel_a(a, s):
     s['res'] += a.xpath('./h1')[0].text.strip() + s['line_break']
     s['res'] += full_url(a.get('href'), context_url = s['base_url'])
     s['res'] += s['par_sep']
+
 
 def process_a(a, s):
     s['res'] += '[%s][%d]' % (innerHTML(a), s['ref_counter'])
@@ -69,6 +76,7 @@ def process_a(a, s):
     }
     s['references'].append(ref)
     s['ref_counter'] += 1
+
 
 # TODO: make it in some other way
 def ret_span(span, orig_s):
@@ -84,6 +92,7 @@ def ret_span(span, orig_s):
     }
     process_toplevel_p(span, s)
     return s['res'].strip()
+
 
 def process_span(span, s):
     # TODO: extract number, don't reenum via 'fn_counver'
@@ -119,6 +128,7 @@ def process_strong(strong, deep_level, s):
             s['res'] += child.tail or ''
     s['res'] += '**' if (deep_level % 2 == 0) else '__'
 
+
 # TODO: deduplicate code
 def process_em(em, deep_level, s):
     s['res'] += '*' if (deep_level % 2 == 0) else '_'
@@ -140,11 +150,13 @@ def process_em(em, deep_level, s):
             s['res'] += child.tail or ''
     s['res'] += '*' if (deep_level % 2 == 0) else '_'
 
+
 def maybe_formula(par):
     if par.lstrip().startswith(r'\[') and par.rstrip().endswith(r'\]'):
         return '$$ ' + par.strip()[2:-2].strip() + ' $$'
     else:
         return par
+
 
 def process_toplevel_p(p, s):
     is_question = 'id' in p.attrib and p.attrib['id'] == 'question'
@@ -176,6 +188,7 @@ def process_toplevel_p(p, s):
         s['res'] += s['par_sep']
     process_footnotes(s)
 
+
 def process_toplevel_img(img, s):
     url = full_url(img.get('src'), context_url = s['base_url']),
     s['res'] += '![](TODO "%s")' % img.get('title') + s['line_break']
@@ -184,11 +197,13 @@ def process_toplevel_img(img, s):
     s['res'] += '[/labels]' + s['line_break']
     s['res'] += 'render: ![](%s)' % url + s['par_sep']
 
+
 def postprocess_references(s):
     for ref in s['references']:
         # TODO: check for spaces and so on
         title = get_title(ref['url'])
         s['res'] += '[%s]: %s "%s"' % (ref['num'], ref['url'], title) + s['par_sep']
+
 
 url = 'http://what-if.xkcd.com'
 html = get_page(url, utf8 = True)

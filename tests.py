4a1732fe9ae9b345192ebb2a5dbbe6c7ecd06ce8
Alexander Turenko <totktonada.ru@gmail.com>
2015-04-17 00:46:05 +0300

    Make pylint almost happy

--- tests.py
+++ tests.py
@@ -1,31 +1,38 @@
 #!/usr/bin/python3
 
+""" Unit testing module for what-if parser. """
 
-from what_if_parse import *
+
+from what_if_parse import process_article
 import unittest
 
 
 class ParserTests(unittest.TestCase):
-    url = 'TESTING'
-    html_tmpl = '<html><body><article>%s</article></body></html>'
+    """ Set of tests for HTML to markdown parsing. """
+    PAGE_URL = 'TESTING'
+    HTML_TMPL = '<html><body><article>%s</article></body></html>'
 
-    def do_check(self, article_html, md):
-        html = ParserTests.html_tmpl % article_html
-        processed = process_article(ParserTests.url, html)[1]
-        self.assertEqual(processed, md)
+    def do_check_equal(self, article_html, article_md):
+        """ Wrap HTML paragraps into the template, parse and equal check."""
+        html = ParserTests.HTML_TMPL % article_html
+        processed = process_article(ParserTests.PAGE_URL, html)[1]
+        self.assertEqual(processed, article_md)
 
     def setUp(self):
+        """ Setting up, nothing to do. """
         pass
 
     def test_italic_1(self):
+        """ Test a italic text inside a italic text. """
         article_html = '<p><em>abc<em>012</em>def</em></p>'
-        md = '*abc_012_def*'
-        self.do_check(article_html, md)
+        article_md = '*abc_012_def*'
+        self.do_check_equal(article_html, article_md)
 
     def test_bold_italic_1(self):
+        """ Test a bold text inside a italic text. """
         article_html = '<p><em>abc<strong>012</strong>def</em></p>'
-        md = '*abc**012**def*'
-        self.do_check(article_html, md)
+        article_md = '*abc**012**def*'
+        self.do_check_equal(article_html, article_md)
 
 
 if __name__ == '__main__':
--- what_if_parse.py
+++ what_if_parse.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 
+""" Main what-if downloading and parsing module
+
+The module defines set of functions for downloading and parsing articles from
+'http://what-if.xkcd.com'.
+
+"""
+
 # TODO's residence:
 # * cross OS packaging
-# * pylint
-# * documenting (README, docstring)
+# * documenting (README)
 # * --quiet|-q option for disabling progress reports
 
 import sys
@@ -17,55 +23,75 @@ EXIT_GET_PAGE_ERROR = 2
 
 
 class GetPageError(Exception):
+    """ The exception raised when smth went wrong in 'get_page' function. """
     pass
 
 
 def get_page(url, utf8=False):
+    """ Get HTML page or raise GetPageError exception.
+
+    If content-type header isn't 'text/html' the exception raised as well as
+    when download error occured.
+
+    """
+    # Disable pylint error message for 'requests.code.ok':
+    # > Instance of 'LookupDict' has no 'ok' member (no-member)
+    # pylint: disable=E1101
     try:
-        r = requests.get(url, headers={'Accept': 'text/html'})
-    except (RequestException, BaseHTTPError) as e:
+        req = requests.get(url, headers={'Accept': 'text/html'})
+    except (RequestException, BaseHTTPError):
         print('[get_page] Exception occured at http request performing: ' \
             + url, file=sys.stderr)
         raise GetPageError()
-    if r.status_code != requests.codes.ok:
+    if req.status_code != requests.codes.ok:
         print('[get_page] HTTP status code: %d; url: %s' % \
-            (r.status_code, url), file=sys.stderr)
+            (req.status_code, url), file=sys.stderr)
         raise GetPageError()
     if utf8:
-        r.encoding = 'utf-8'
-    content_type = r.headers['content-type']
+        req.encoding = 'utf-8'
+    content_type = req.headers['content-type']
     if content_type.startswith('text/html'):
-        return r.text
+        return req.text
     else:
         print('[get_page] Content type "%s" != "text/html"; url: %s' % \
             (content_type, url), file=sys.stderr)
         raise GetPageError()
 
 
-def get_title(ref, refs_cnt, default_res='TODO'):
-    header = '[get_title %d/%d] ' % (ref['num'], refs_cnt)
-    print(header + 'Download page from %s' % ref['url'], file=sys.stderr)
+def get_title(reference, refs_cnt, default_res='TODO'):
+    """ Get title of page by its url.
+
+    Download page from reference['url'] and extract title. If some error
+    occured, 'default_res' value returned.
+
+    """
+    header = '[get_title %d/%d] ' % (reference['num'], refs_cnt)
+    print(header + 'Download page from %s' % reference['url'], file=sys.stderr)
     try:
-        html = get_page(ref['url'])
-    except GetPageError as e:
+        html = get_page(reference['url'])
+    except GetPageError:
         print(header + 'Skip title extracting: get page error or wrong html page', \
             file=sys.stderr)
         return default_res
     print(header + 'Extract title from the page', file=sys.stderr)
     doc = lxml.html.document_fromstring(html)
-    ts = doc.xpath('//title')
-    if len(ts) == 0:
+    titles = doc.xpath('//title')
+    if len(titles) == 0:
         return default_res
     res = ''
-    for line in ts[0].text.split('\n'):
+    for line in titles[0].text.split('\n'):
         line = line.strip()
         if len(line) > 0:
             res += line + ' '
     return res.rstrip()
 
 
-# assume 'context_url' are full url
 def full_url(url, context_url):
+    """ Get full (absolute) URL from arbitrary URL and page where it placed.
+
+    Assume 'context_url' are full url.
+
+    """
     proto, tail = context_url.split(':', 1)
     context_base = proto + '://' + tail.lstrip('/').split('/', 1)[0]
 
@@ -84,8 +110,12 @@ def full_url(url, context_url):
         raise NameError('bad url in \'full_url\':\nurl: ' + url + '\n')
 
 
-# http://stackoverflow.com/a/24151860/1598057
-def innerHTML(node, strip=True):
+def inner_html(node, strip=True):
+    """ Get inner HTML from lxml node.
+
+    http://stackoverflow.com/a/24151860/1598057
+
+    """
     res = node.text or ''
     for child in node:
         res += lxml.html.tostring(child, encoding='unicode')
@@ -94,59 +124,64 @@ def innerHTML(node, strip=True):
     return res
 
 
-def pop_footnotes(s):
+def pop_footnotes(state):
+    """ Get preparsed footnotes text and flush it. """
     res = ''
-    for fn in s['footnotes']:
-        res += '[^%s]: %s' % (fn['num'], fn['body']) + s['par_sep']
-    s['footnotes'] = []
+    for footnote in state['footnotes']:
+        res += '[^%s]: %s' % (footnote['num'], footnote['body']) + \
+            state['par_sep']
+    state['footnotes'] = []
     return res
 
 
-def process_toplevel_a(a, s):
-    title = a.xpath('./h1')[0].text.strip()
-    url = full_url(a.get('href'), context_url=s['base_url'])
+def process_toplevel_a(a_elem, state):
+    """ Process toplevel <a/> element (child of <article/>). """
+    title = a_elem.xpath('./h1')[0].text.strip()
+    url = full_url(a_elem.get('href'), context_url=state['base_url'])
     num = int(url.rstrip('/').rsplit('/', 1)[1])
 
-    s['slug'] = str(num).rjust(3, '0') + '-' + title.lower() \
+    state['slug'] = str(num).rjust(3, '0') + '-' + title.lower() \
         .replace(' ', '-').replace('.', '')
 
-    res = title + s['line_break']
-    res += url + s['par_sep']
+    res = title + state['line_break']
+    res += url + state['par_sep']
     return res
 
 
-def process_a(a, s):
-    res = '[%s][%d]' % (innerHTML(a), s['ref_counter'])
+def process_a(a_elem, state):
+    """ Process inline <a/> element (somewhere under <article/>). """
+    res = '[%s][%d]' % (inner_html(a_elem), state['ref_counter'])
     ref = {
-        'num': s['ref_counter'],
-        'url': full_url(a.get('href'), context_url=s['base_url']),
+        'num': state['ref_counter'],
+        'url': full_url(a_elem.get('href'), context_url=state['base_url']),
     }
-    s['references'].append(ref)
-    s['ref_counter'] += 1
+    state['references'].append(ref)
+    state['ref_counter'] += 1
     return res
 
 
-def process_childs(elem, s, em_mark='*', strong_mark='**'):
+def process_childs(elem, state, em_mark='*', strong_mark='**'):
+    """ Process some inline HTML element (somewhere under <article/>). """
     res = elem.text or ''
     for child in elem:
         tail_added = False
         if child.tag == 'em':
             child_mark = '_' if em_mark == '*' else '*'
             res += em_mark
-            res += process_childs(child, s, em_mark=child_mark)
+            res += process_childs(child, state, em_mark=child_mark)
             res += em_mark
         elif child.tag == 'strong':
             child_mark = '__' if strong_mark == '**' else '**'
             res += strong_mark
-            res += process_childs(child, s, strong_mark=child_mark)
+            res += process_childs(child, state, strong_mark=child_mark)
             res += strong_mark
         elif child.tag == 'a':
-            res += process_a(child, s)
+            res += process_a(child, state)
         elif child.tag == 'span':
-            res += process_span(child, s)
+            res += process_span(child, state)
         elif child.tag == 'sup':
             res += '<sup>'
-            res += process_childs(child, s)
+            res += process_childs(child, state)
             res += '</sup>'
         else:
             res += lxml.html.tostring(child, encoding='unicode')
@@ -156,73 +191,90 @@ def process_childs(elem, s, em_mark='*', strong_mark='**'):
     return res
 
 
-def process_span(span, s):
+def process_span(span, state):
+    """ Process inline <span/> element (somewhere under <article/>).
+
+    Detect footnotes (its formatted as <span/>) and save its into 'state',
+    later it will extracted in 'pop_footnotes' function. If span element isn't
+    footnote, then return raw its content.
+
+    """
     if span.get('class') != 'ref':
         return lxml.html.tostring(span, encoding='unicode')
 
-    res = '[^%s]' % s['fn_counter']
+    res = '[^%s]' % state['fn_counter']
     refbody = span.xpath('./span[@class="refbody"]')[0]
     # TODO: formulas in footnotes?
-    refbody_parsed = process_childs(refbody, s).strip()
+    refbody_parsed = process_childs(refbody, state).strip()
     footnote = {
-        'num': s['fn_counter'],
+        'num': state['fn_counter'],
         'body': refbody_parsed,
     }
-    s['footnotes'].append(footnote)
-    s['fn_counter'] += 1
+    state['footnotes'].append(footnote)
+    state['fn_counter'] += 1
     return res
 
 
 def maybe_formula(par):
+    """ Returns a non-inline formula or empty string if it isn't detected. """
     if par.lstrip().startswith(r'\[') and par.rstrip().endswith(r'\]'):
         return '$$ ' + par.strip()[2:-2].strip() + ' $$'
     else:
         return ''
 
 
-def process_toplevel_p(p, s):
-    is_question = 'id' in p.attrib and p.attrib['id'] == 'question'
-    is_attribute = 'id' in p.attrib and p.attrib['id'] == 'attribute'
+def process_toplevel_p(p_elem, state):
+    """ Process toplevel <p/> element (child of <article/>).
+
+    Detect a question (citate) <p/> element and a non-inline formula. Dump
+    paragraph footnotes after it.
+
+    """
+    is_question = 'id' in p_elem.attrib and p_elem.attrib['id'] == 'question'
+    is_attribute = 'id' in p_elem.attrib and p_elem.attrib['id'] == 'attribute'
     res = ''
     if is_question or is_attribute:
         res += '> '
     # TODO: check for formula only for entire fragment
-    res += maybe_formula(p.text or '')
-    res += process_childs(p, s)
+    res += maybe_formula(p_elem.text or '')
+    res += process_childs(p_elem, state)
     if is_question:
-        res += s['line_break'] + '>' + s['line_break']
+        res += state['line_break'] + '>' + state['line_break']
     else:
-        res += s['par_sep']
-    res += pop_footnotes(s)
+        res += state['par_sep']
+    res += pop_footnotes(state)
     return res
 
 
-def process_toplevel_img(img, s):
-    url = full_url(img.get('src'), context_url=s['base_url'])
+def process_toplevel_img(img, state):
+    """ Process toplevel <img/> element (child of <article/>). """
+    url = full_url(img.get('src'), context_url=state['base_url'])
     img_file = url.rstrip('/').rsplit('/', 1)[1]
     img_name, img_ext = img_file.rsplit('.')
     img_file_ru = img_name + '_ru.' + img_ext
 
-    res = '![](/uploads/%s/%s "%s")' % (s['slug'], img_file_ru, \
-        img.get('title')) + s['line_break']
-    res += '[labels]' + s['line_break']
-    res += 'TODO' + s['line_break']
-    res += '[/labels]' + s['line_break']
-    res += 'render: ![](%s)' % url + s['par_sep']
+    res = '![](/uploads/%s/%s "%s")' % (state['slug'], img_file_ru, \
+        img.get('title')) + state['line_break']
+    res += '[labels]' + state['line_break']
+    res += 'TODO' + state['line_break']
+    res += '[/labels]' + state['line_break']
+    res += 'render: ![](%s)' % url + state['par_sep']
     return res
 
 
-def postprocess_references(s):
+def postprocess_references(state):
+    """ Format and return preparsed references. """
     res = ''
-    refs_cnt = len(s['references'])
-    for ref in s['references']:
-        title = get_title(ref, refs_cnt)
-        res += '[%s]: %s "%s"' % (ref['num'], ref['url'], title) + \
-            s['par_sep']
+    refs_cnt = len(state['references'])
+    for reference in state['references']:
+        title = get_title(reference, refs_cnt)
+        res += '[%s]: %s "%s"' % (reference['num'], reference['url'], \
+            title) + state['par_sep']
     return res
 
 
 def new_parser(url):
+    """ Return new (clean) parser state. """
     return {
         'slug': None,
         'base_url': url,
@@ -236,11 +288,18 @@ def new_parser(url):
 
 
 def process_article(url, html):
+    """ Process all toplevel article elements (childs of <article/>).
+
+    In markdown references added after all text paragraps (HTML toplevel
+    elements representation). Returns article HTML (cutted from entire HTML),
+    markdown and a 'slug' (king of article id, for using in filenames).
+
+    """
     doc = lxml.html.document_fromstring(html)
     article = doc.xpath('//body//article')[0]
-    article_html = innerHTML(article)
+    article_html = inner_html(article)
 
-    parser_state = new_parser(url)
+    state = new_parser(url)
 
     res = ''
     childs_cnt = len(article)
@@ -254,18 +313,24 @@ def process_article(url, html):
         print('Processed %d/%d top level elements' % (childs_processed, \
             childs_cnt), file=sys.stderr)
         if child.tag in func_dict.keys():
-            res += func_dict[child.tag](child, parser_state)
+            res += func_dict[child.tag](child, state)
         else:
             print('Unexpected toplevel element: ' + child.tag, file=sys.stderr)
         childs_processed += 1
 
     print('Postprocessing references...', file=sys.stderr)
-    res += postprocess_references(parser_state)
+    res += postprocess_references(state)
 
-    return article_html, res.strip(), parser_state['slug']
+    return article_html, res.strip(), state['slug']
 
 
-if __name__ == '__main__':
+def main():
+    """ Main actions sequence.
+
+    Get and check arguments, call downloading and parsing functions, write
+    results to files.
+
+    """
     if len(sys.argv) == 1:
         url = 'http://what-if.xkcd.com'
     elif len(sys.argv) == 2 and sys.argv[1].isdigit():
@@ -277,7 +342,7 @@ if __name__ == '__main__':
     print('Download article from %s' % url, file=sys.stderr)
     try:
         html = get_page(url, utf8=True)
-    except GetPageError as e:
+    except GetPageError:
         print('Error when getting page, exitting...', file=sys.stderr)
         exit(EXIT_GET_PAGE_ERROR)
 
@@ -286,11 +351,15 @@ if __name__ == '__main__':
     html_file = slug + '.html'
     md_file = slug + '.md'
 
-    with open(html_file, 'w', encoding='utf-8') as f:
+    with open(html_file, 'w', encoding='utf-8') as html_file_fd:
         print('Write article in html to file %s' % html_file, \
             file=sys.stderr)
-        print(article_html, file=f)
-    with open(md_file, 'w', encoding='utf-8') as f:
+        print(article_html, file=html_file_fd)
+    with open(md_file, 'w', encoding='utf-8') as md_file_fd:
         print('Write article in markdown to file %s' % md_file, \
             file=sys.stderr)
-        print(article_md, file=f)
+        print(article_md, file=md_file_fd)
+
+
+if __name__ == '__main__':
+    main()
